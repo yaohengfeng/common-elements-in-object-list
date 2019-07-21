5a3699883a9cd512b06ee9513a32e9c96584c159
Bo Zhang <zhangbodut@gmail.com>
2019-07-21 19:15:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Collection: 求对象列表的公共元素
+
+请解决[`Main`类](https://github.com/hcsp/common-elements-in-object-list/blob/master/src/main/com/github/hcsp/collection/Main.java)中的问题。
+
+-----
+注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
+- [src/main/com/github/hcsp/collection/Main.java](https://github.com/hcsp/common-elements-in-object-list/blob/master/src/main/com/github/hcsp/collection/Main.java)
+-----
+
+
+完成题目有困难？不妨来看看[写代码啦的相应课程](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d)吧！
+
+回到[写代码啦的题目](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d/quizzes/6c87ef57-7f06-4af2-9112-86dd27ff099d)，继续挑战！
---
+++ src/test/java/com/github/hcsp/collection/MainTest.java
@@ -0,0 +1,18 @@
+package com.github.hcsp.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        List<Main.Person> list1 = Arrays.asList(new Main.Person("张学友"), new Main.Person("周杰伦"));
+        List<Main.Person> list2 = Arrays.asList(new Main.Person("周润发"), new Main.Person("周杰伦"));
+        Assertions.assertEquals(
+                new HashSet(Arrays.asList(new Main.Person("周杰伦"))),
+                Main.commonElementsIn(list1, list2));
+    }
+}
