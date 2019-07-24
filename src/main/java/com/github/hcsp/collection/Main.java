6847b45317b014554c0de684abd32ad7319dd9b3
JayChenFE <chenjiehwy@163.com>
2019-07-24 11:45:30 +0800

    按需引入包

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
