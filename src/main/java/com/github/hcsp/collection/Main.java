026dfcce910099a30819363b60b1a84556072c19
zhangyongwang <20225614@qq.com>
2019-07-30 17:31:49 +0800

    降低复杂度

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -11,7 +11,10 @@ import java.util.Arrays;
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
     public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
-
+        Set<Person> set1 = new HashSet<>(a);
+        Set<Person> set2 = new HashSet<>(b);
+        set1.retainAll(set2);
+        return new HashSet<>(set1);
     }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
