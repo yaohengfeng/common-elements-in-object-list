23c5bc40faf8fb42f3c4a6d80632c4628dea6802
dflk <904885508@qq.com>
2019-08-11 22:16:58 +0800

    'why'

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,17 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> seta = new HashSet<>(a);
+        Set<Person> setb = new HashSet<>(b);
+        seta.retainAll(setb);
+//        set.add(a);
+//        Set<Person> set = new HashSet(a);
+        return seta;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -23,11 +28,26 @@ public class Main {
         public void setName(String name) {
             this.name = name;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
     }
 
     public static void main(String[] args) {
-        List<Person> list1 = Arrays.asList(new Person("张学友"), new Person("周杰伦"));
+
         List<Person> list2 = Arrays.asList(new Person("周润发"), new Person("周杰伦"));
+        List<Person> list1 = Arrays.asList(new Person("张学友"), new Person("周杰伦"));
         System.out.println(commonElementsIn(list1, list2));
     }
+
 }
