0cce30093e8a7d00558d8a2172876a7aaa8f6337
Zain <wangziyi.2019@bytedance.com>
2019-08-11 13:21:38 +0800

    Fix hashCode Override

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -37,6 +37,12 @@ public class Main {
         }
 
         @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+
+        @Override
         public boolean equals(Object o) {
             if (this == o) {
                 return true;
