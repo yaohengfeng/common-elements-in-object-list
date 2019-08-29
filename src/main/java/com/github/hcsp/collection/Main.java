7e2bfb4d39c4fe4d8a232e7fc17ed93621accfe1
kungua <52815012+kungua@users.noreply.github.com>
2019-08-29 13:24:33 +0800

    common elements in object (#31)
    common elements in object (#31)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,27 +1,55 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        List<Person> copyA = new ArrayList<>(a);
+        List<Person> copyB = new ArrayList<>(b);
+        copyB.retainAll(copyA);
+        return new HashSet<>(copyB);
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
         private String name;
 
-        public Person(String name) {
+        Person(String name) {
             this.name = name;
         }
 
-        public String getName() {
+        String getName() {
             return name;
         }
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object that) {
+            if (!(that instanceof Person)) {
+                return false;
+            }
+            return this.name.equals(((Person) that).getName());
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 
