42d7a4e3c4c1e876550aa0fe1b37610685509533
EthanWang <46627960+EthanWXL@users.noreply.github.com>
2019-09-04 16:17:35 +0800

    求对象列表的公共元素 (#32)
    求对象列表的公共元素 (#32)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,14 +1,26 @@
 package com.github.hcsp.collection;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> personSet = new HashSet<>();
+        for (Person person : a
+                ) {
+            if (b.contains(person)) {
+                personSet.add(person);
+            }
+        }
+        return personSet;
+    }
 
-    // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
+    /**Person类，如果两个Person对象的name相等，则认为这两个对象相等。*/
     public static class Person {
         private String name;
 
@@ -22,6 +34,31 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(name);
         }
     }
 
