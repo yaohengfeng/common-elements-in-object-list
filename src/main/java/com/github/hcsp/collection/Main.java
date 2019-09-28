c7e6058cbac419634529d36e5c5d8eb347a567e5
Hank <hankhu163@foxmail.com>
2019-09-28 10:30:45 +0800

    Stream() + equals() + hasCode() (#40)
    Stream() + equals() + hasCode() (#40)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -2,11 +2,18 @@ package com.github.hcsp.collection;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> set = a.stream().filter(
+                aElement -> b.contains(aElement))
+                .collect(Collectors.toSet());
+        return set;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -15,13 +22,23 @@ public class Main {
         public Person(String name) {
             this.name = name;
         }
+        // get set 方法用不上
 
-        public String getName() {
-            return name;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
         }
 
-        public void setName(String name) {
-            this.name = name;
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 
