ff298b4d60675c60c24e398104975d4c880d22bb
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-10-07 15:03:47 +0800

    LinkedHashSet (#43)
    LinkedHashSet (#43)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,19 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> set1 = new HashSet<>(a);
+        Set<Person> set2 = new HashSet<>(b);
+        set1.retainAll(set2);
+        return set1;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -20,8 +27,27 @@ public class Main {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o){
+                return true;
+            }
+            if (!(o instanceof Person)){
+                return false;
+            }
+            return this.name.equals(((Person) o).getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
         }
     }
 
