b580f40fdf6637e7db8d26334e1e56b4e16cdd57
MrRobot <584937529@qq.com>
2019-10-10 23:14:03 +0800

    求对象列表的公共元素

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
@@ -36,8 +40,12 @@ public class Main {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Person person = (Person) o;
             return Objects.equals(name, person.name);
         }
