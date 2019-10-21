044cb9af4f557c5518396fd52b2ccdc81252f9bc
phlvance <39873702+phlvance@users.noreply.github.com>
2019-10-21 20:58:13 +0800

    common elements in object list (#52)
    common elements in object list (#52)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,15 +1,17 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        List<Person> common = new ArrayList<>(a);
+        common.retainAll(b);
+        return new HashSet<>(common);
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
-    public static class Person {
+    public static class Person implements Comparable<Person> {
         private String name;
 
         public Person(String name) {
@@ -22,6 +24,36 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(name);
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            return name.compareTo(o.name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
         }
     }
 
