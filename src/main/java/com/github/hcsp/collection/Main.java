e11898f7cc976f55c1788bb748199a70b71a2450
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-11-25 17:24:42 +0800

    求对象列表的公共元素 (#60)
    求对象列表的公共元素 (#60)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,23 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> person = new HashSet<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (a.get(i).equals(b.get(j))) {
+                    person.add(b.get(j));
+                    break;
+                }
+            }
+        }
+        return person;
+    }
+
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -23,11 +34,22 @@ public class Main {
         public void setName(String name) {
             this.name = name;
         }
-    }
 
-    public static void main(String[] args) {
-        List<Person> list1 = Arrays.asList(new Person("张学友"), new Person("周杰伦"));
-        List<Person> list2 = Arrays.asList(new Person("周润发"), new Person("周杰伦"));
-        System.out.println(commonElementsIn(list1, list2));
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Person person = (Person) obj;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash();
+        }
     }
 }
