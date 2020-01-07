5b23c05a0979e53381c34cb9bbc0aa2ac8ec413f
zaker-666 <1075151259china@gmail.com>
2020-01-07 23:07:41 +0800

    compare two person (#77)
    compare two person (#77)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,15 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        ArrayList<Person> personsOne = new ArrayList<>(a);
+        ArrayList<Person> personsTwo = new ArrayList<>(b);
+        personsOne.retainAll(personsTwo);
+        return new HashSet<>(personsOne);
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -22,6 +25,20 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Person) {
+                Person person = (Person) obj;
+                return this.getName().equals(person.getName());
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return name != null ? name.hashCode() : 0;
         }
     }
 
